13e905979969afcadc70d306a67372498cca70d1
André Bauer <andre@recogni.com>
2023-07-19 17:55:08 +0200

    config on gcs

--- python-sdk/nuscenes/eval/detection/config.py
+++ python-sdk/nuscenes/eval/detection/config.py
@@ -4,6 +4,7 @@
 import json
 import os
 
+import fsspec
 from nuscenes.eval.detection.data_classes import DetectionConfig
 
 
@@ -16,13 +17,13 @@ def config_factory(configuration_name: str) -> DetectionConfig:
     """
 
     # Check if config exists.
-    this_dir = os.path.dirname(os.path.abspath(__file__))
-    cfg_path = os.path.join(this_dir, 'configs', '%s.json' % configuration_name)
-    assert os.path.exists(cfg_path), \
+    fs = fsspec.filesystem("gcs")
+    cfg_path = f"gs://reco-ds/nuscenes/nuscenes-devkit/eval/detection/configs/{configuration_name}.json"
+    assert fs.exists(cfg_path), \
         'Requested unknown configuration {}'.format(configuration_name)
 
     # Load config file and deserialize it.
-    with open(cfg_path, 'r') as f:
+    with fs.open(cfg_path, 'r') as f:
         data = json.load(f)
     cfg = DetectionConfig.deserialize(data)
 
